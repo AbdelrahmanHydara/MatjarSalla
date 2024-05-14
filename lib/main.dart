6ce5e24964e512ee986cfc31fb7225c27280ecf0
Abdelrahman Nasser <108473379+AbdelrahmanHydara@users.noreply.github.com>
2024-05-14 21:00:10 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:matjar_salla/core/services/services.dart';
+import 'package:matjar_salla/shared/helper/remote/dio.dart';
+import 'core/constants/routes.dart';
+import 'core/localization/change_locale.dart';
+import 'core/localization/translation.dart';
+import 'package:get/get.dart';
+import 'shared/bindings/initial_bindings.dart';
+
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialServices();
+  await DioHelper.init();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    MyLocaleController controller = Get.put(MyLocaleController());
+
+    return GetMaterialApp(
+      theme: controller.appTheme,
+      locale: controller.initialLanguage,
+      getPages: routes,
+      initialBinding: InitialBindings() ,
+      translations: MyTranslation(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
